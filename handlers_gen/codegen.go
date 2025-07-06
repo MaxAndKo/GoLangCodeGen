84d7c47339f0c220de1e9168041a93823dbb5c69
maxandko <maksim@konchalov.ru>
2025-07-06 23:06:25 +0300

    Stage 1

--- handlers_gen/codegen.go
+++ handlers_gen/codegen.go
@@ -1,3 +1,84 @@
 package main
 
-// код писать тут
+import (
+	"encoding/json"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"strings"
+)
+
+type Enum struct {
+	Values  []string
+	Default string
+}
+
+type ValidatorArgs struct {
+	Required  bool
+	ParamName string
+	Enum      Enum
+	min       int
+	max       int
+}
+
+type Api struct {
+	Url    string
+	Auth   bool
+	Method string
+}
+
+func main() {
+	set := token.NewFileSet()
+	f, err := parser.ParseFile(set, os.Args[1], nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+	res, err := os.Create(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+
+	apigens := make([]Api, 0)
+	for _, decl := range f.Decls {
+		funcDecl, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			fmt.Println("It is not func. Skip")
+			continue
+		}
+
+		apigenString, containsApigen := getApigenString(funcDecl.Doc)
+		if !containsApigen {
+			fmt.Println("It is not apigen. Skip")
+			continue
+		}
+
+		apigen := new(Api)
+		substr := apigenString[strings.IndexRune(apigenString, '{'):]
+		fmt.Println("Found apigen: ", substr)
+		bytes := []byte(substr)
+		err := json.Unmarshal(bytes, apigen)
+		if err != nil {
+			panic(err)
+		}
+		apigens = append(apigens, *apigen)
+	}
+
+	fmt.Fprintln(res, "")
+	fmt.Println(apigens)
+}
+
+func getApigenString(doc *ast.CommentGroup) (string, bool) {
+	if doc == nil {
+		return "", false
+	}
+
+	comments := doc.List
+	for _, comment := range comments {
+		if strings.Contains(comment.Text, "apigen") {
+			return comment.Text, true
+		}
+	}
+	return "", false
+}
